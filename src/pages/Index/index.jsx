f4a8ba0aa7300fd425e7d1545a1c34e2c61151c6
dingff <dingff0923@foxmail.com>
2023-04-13 10:39:35 +0800

    chore(deps): upgrade umi and antd

--- src/pages/Index/index.jsx
+++ src/pages/Index/index.jsx
@@ -1,5 +1,13 @@
+import { Button } from 'antd'
+import { useState } from 'react'
+
 export default function Index() {
+  const [count, setCount] = useState(0)
   return (
-    <div>666</div>
+    <div>
+      <div>你点击了{count}次</div>
+
+      <Button onClick={() => setCount(count + 1)}>点击</Button>
+    </div>
   )
 }
